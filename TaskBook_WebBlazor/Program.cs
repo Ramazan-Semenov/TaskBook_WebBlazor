094480281bd176710976f5f5020fd107b8c58a30
semenov ramazan <rombik.s@inbox.ru>
2025-01-04 03:28:37 +0300

    xc

--- TaskBook_WebBlazor/Program.cs
+++ TaskBook_WebBlazor/Program.cs
@@ -26,14 +26,11 @@ namespace TaskBook_WebBlazor
             {
                 app.UseExceptionHandler("/Error");
                 // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
-                app.UseHsts();
             }
+            app.UseHsts();
 
-            if (app.Environment.IsDevelopment())
-            {
-                app.UseSwagger();
-                app.UseSwaggerUI();
-            };
+            app.UseSwagger();
+            app.UseSwaggerUI();
 
             app.UseHttpsRedirection();
 
